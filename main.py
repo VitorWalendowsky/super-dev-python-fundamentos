0d04b4aa91dc4517feaa973364b9ebd6ed14722b
Vitor Walendowsky <vitor.walendowsky@gmail.com>
2025-10-29 19:34:05 -0300

    Primeiro commit - upload do projeto

---
+++ main.py
@@ -0,0 +1,9 @@
+from src import variaveis
+
+def main():
+    variaveis.exemplo_strings()
+
+if __name__ == "__main__":
+    main() #executar uma função chamada de main (principal)
+
+# Ctrl + J > py main.py no linux python main.py no windows
---
+++ src/variaveis.py
@@ -0,0 +1,34 @@
+def exemplo_strings():
+    #variaveis o tipo string
+    nome_inquilino = "João"
+    sobrenome_inquilino = "da Silva"
+
+    #Concatenacão (junta)
+    nome_completo = str = nome_inquilino + " " + sobrenome_inquilino
+    print(nome_completo)
+
+    #criar outra funcao para armazenar os dados do paciente
+    # nome do paciente
+    # cidade natal
+    # tipo sanguineo
+    # apresentar tudo
+    # Nomenclatura de uma funcao/metodo sempre deve ser uma ação (verbo)
+    # exemplo: apresentar_dados_paciente()
+
+def apresentar_dados_paciente():
+    nome_do_paciente = "Maria de Souza"
+    cidade_natal = "Santa Catarina"
+    tipo_sanguineo = "O+"
+    
+    print("Nome:", nome_do_paciente)
+    print("Cidade natal:", cidade_natal)
+    print("Tipo sanguíneo:", tipo_sanguineo)
+
+# Chamando a função
+apresentar_dados_paciente()
+
+
+
+
+   # Ctrl + J > py main.py no linux python3 main.py
+
